b012b167c8850f65efad4bc8fb12871395ff2b7e
Murray Cumming <murrayc@murrayc.com>
2017-05-11 22:08:15 +0200

    examples: cc-internet: Remove "using namespace boost".

--- example/cc-internet.cpp
+++ example/cc-internet.cpp
@@ -16,25 +16,24 @@
 int
 main()
 {
-  using namespace boost;
   GraphvizGraph g;
-  read_graphviz("figs/cc-internet.dot", g);
+  boost::read_graphviz("figs/cc-internet.dot", g);
 
-  std::vector<int> component(num_vertices(g));
+  std::vector<int> component(boost::num_vertices(g));
 
-  connected_components
-    (g, make_iterator_property_map(component.begin(),
-                                   get(vertex_index, g), component[0]));
+  boost::connected_components
+    (g, boost::make_iterator_property_map(component.begin(),
+                                   boost::get(boost::vertex_index, g), component[0]));
 
-  auto vertex_attr_map = get(vertex_attribute, g);
+  auto vertex_attr_map = boost::get(vertex_attribute, g);
   std::string color[] = {
   "white", "gray", "black", "lightgray"};
-  for(const auto& vertex : make_range_pair(vertices(g))) {
+  for(const auto& vertex : make_range_pair(boost::vertices(g))) {
     vertex_attr_map[vertex]["color"] = color[component[vertex]];
     vertex_attr_map[vertex]["style"] = "filled";
     if (vertex_attr_map[vertex]["color"] == "black")
       vertex_attr_map[vertex]["fontcolor"] = "white";
   }
-  write_graphviz("figs/cc-internet-out.dot", g);
+  boost::write_graphviz("figs/cc-internet-out.dot", g);
 
 }
