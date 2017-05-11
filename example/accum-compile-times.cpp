25ce609a75686d018fc9bf72e023eb56540bb643
Murray Cumming <murrayc@murrayc.com>
2017-05-11 11:14:31 +0200

    examples: accum-compile-times: Remove "using namespace boost".

--- example/accum-compile-times.cpp
+++ example/accum-compile-times.cpp
@@ -31,52 +31,50 @@ namespace boost
   BOOST_INSTALL_PROPERTY(vertex, compile_cost);
 }
 
-using namespace boost;
-
-using file_dep_graph2 = adjacency_list<listS, // Store out-edges of each vertex in a std::list
-  listS,                       // Store vertex set in a std::list
-  directedS,                   // The file dependency graph is directed
+using file_dep_graph2 = boost::adjacency_list<boost::listS, // Store out-edges of each vertex in a std::list
+  boost::listS,                       // Store vertex set in a std::list
+  boost::directedS,                   // The file dependency graph is directed
   // vertex properties
-  property<vertex_name_t, std::string,
-  property<vertex_compile_cost_t, float,
-  property<vertex_distance_t, float,
-  property<vertex_color_t, default_color_type>>>>,
+  boost::property<boost::vertex_name_t, std::string,
+  boost::property<boost::vertex_compile_cost_t, float,
+  boost::property<boost::vertex_distance_t, float,
+  boost::property<boost::vertex_color_t, boost::default_color_type>>>>,
   // an edge property
-  property<edge_weight_t, float>>;
+  boost::property<boost::edge_weight_t, float>>;
 
 int
 main()
 {
   std::ifstream file_in("makefile-dependencies.dat");
-  using size_type = graph_traits<file_dep_graph2>::vertices_size_type;
+  using size_type = boost::graph_traits<file_dep_graph2>::vertices_size_type;
   size_type n_vertices;
   file_in >> n_vertices;        // read in number of vertices
 #if defined(BOOST_MSVC) && BOOST_MSVC <= 1300
   // std::istream_iterator causes trouble with VC++
-  std::vector<vertex_t> id2vertex;
+  std::vector<boost::vertex_t> id2vertex;
   file_dep_graph2 g;
   for (std::size_t i = 0; i < n_vertices; ++i)
     id2vertex.emplace_back(add_vertex(g));
   std::pair<size_type, size_type> p;
   while (file_in >> p) 
-    add_edge(id2vertex[p.first], id2vertex[p.second], g);
+    boost::add_edge(id2vertex[p.first], id2vertex[p.second], g);
 #else
   std::istream_iterator<std::pair<size_type, size_type>>
     input_begin(file_in), input_end;
   file_dep_graph2 g(input_begin, input_end, n_vertices);
 #endif
 
-  auto name_map = get(vertex_name, g);
-  auto compile_cost_map = get(vertex_compile_cost, g);
+  auto name_map = boost::get(boost::vertex_name, g);
+  auto compile_cost_map = boost::get(boost::vertex_compile_cost, g);
 
   std::ifstream name_in("makefile-target-names.dat");
   std::ifstream compile_cost_in("target-compile-costs.dat");
-  for(const auto& vertex : make_range_pair(vertices(g))) {
+  for(const auto& vertex : make_range_pair(boost::vertices(g))) {
     name_in >> name_map[vertex];
     compile_cost_in >> compile_cost_map[vertex];
   }
 
-  const auto [ci, ci_end] = get_property_iter_range(g, vertex_compile_cost);
+  const auto [ci, ci_end] = boost::get_property_iter_range(g, boost::vertex_compile_cost);
   std::cout << "total (sequential) compile time: "
     << std::accumulate(ci, ci_end, 0.0) << std::endl;
 
