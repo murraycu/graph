7598f92bcc0e16bb653bed2c83432bd72a2577ba
Murray Cumming <murrayc@murrayc.com>
2017-05-11 22:39:08 +0200

    examples: default-constructor: Remove "using namespace boost".

--- example/default-constructor.cpp
+++ example/default-constructor.cpp
@@ -9,12 +9,10 @@
 #include <fstream>
 #include <boost/graph/adjacency_list.hpp>
 
-using namespace boost;
-
 template <typename Graph> void
 read_graph_file(std::istream & in, Graph & g)
 {
-  using size_type = typename graph_traits<Graph>::vertices_size_type;
+  using size_type = typename boost::graph_traits<Graph>::vertices_size_type;
   size_type n_vertices;
   in >> n_vertices;             // read in number of vertices
   for (size_type i = 0; i < n_vertices; ++i)    // Add n vertices to the graph
@@ -22,7 +20,7 @@ read_graph_file(std::istream & in, Graph & g)
   size_type u, v;
   while (in >> u)               // Read in pairs of integers as edges
     if (in >> v)
-      add_edge(u, v, g);
+      boost::add_edge(u, v, g);
     else
       break;
 }
@@ -31,16 +29,16 @@ read_graph_file(std::istream & in, Graph & g)
 int
 main()
 {
-  using graph_type = adjacency_list < listS,       // Store out-edges of each vertex in a std::list
-    vecS,                       // Store vertex set in a std::vector
-    directedS                   // The graph is directed
+  using graph_type = boost::adjacency_list < boost::listS,       // Store out-edges of each vertex in a std::list
+    boost::vecS,                       // Store vertex set in a std::vector
+    boost::directedS                   // The graph is directed
   >;
 
   graph_type g;                 // use default constructor to create empty graph
   std::ifstream file_in("makefile-dependencies.dat");
   read_graph_file(file_in, g);
 
-  assert(num_vertices(g) == 15);
-  assert(num_edges(g) == 19);
+  assert(boost::num_vertices(g) == 15);
+  assert(boost::num_edges(g) == 19);
   return 0;
 }
