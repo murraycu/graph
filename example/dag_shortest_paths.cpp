2c5851114159e26c26de00cb09ad8361dcdf8036
Murray Cumming <murrayc@murrayc.com>
2017-05-11 22:38:15 +0200

    examples: dag_shortest_paths: Remove "using namespace boost".

--- example/dag_shortest_paths.cpp
+++ example/dag_shortest_paths.cpp
@@ -25,41 +25,40 @@
 
 int main()
 {
-  using namespace boost;
-  using graph_t = adjacency_list<vecS, vecS, directedS, 
-    property<vertex_distance_t, int>, property<edge_weight_t, int>>;
+  using graph_t = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, 
+    boost::property<boost::vertex_distance_t, int>, boost::property<boost::edge_weight_t, int>>;
   graph_t g(6);
   enum verts { r, s, t, u, v, x };
   char name[] = "rstuvx";
-  add_edge(r, s, 5, g);
-  add_edge(r, t, 3, g);
-  add_edge(s, t, 2, g);
-  add_edge(s, u, 6, g);
-  add_edge(t, u, 7, g);
-  add_edge(t, v, 4, g);
-  add_edge(t, x, 2, g);
-  add_edge(u, v, -1, g);
-  add_edge(u, x, 1, g);
-  add_edge(v, x, -2, g);
+  boost::add_edge(r, s, 5, g);
+  boost::add_edge(r, t, 3, g);
+  boost::add_edge(s, t, 2, g);
+  boost::add_edge(s, u, 6, g);
+  boost::add_edge(t, u, 7, g);
+  boost::add_edge(t, v, 4, g);
+  boost::add_edge(t, x, 2, g);
+  boost::add_edge(u, v, -1, g);
+  boost::add_edge(u, x, 1, g);
+  boost::add_edge(v, x, -2, g);
 
-  auto d_map = get(vertex_distance, g);
+  auto d_map = boost::get(boost::vertex_distance, g);
 
 #if defined(BOOST_MSVC) && BOOST_MSVC <= 1300
   // VC++ has trouble with the named-parameter mechanism, so
   // we make a direct call to the underlying implementation function.
-  std::vector<default_color_type> color(num_vertices(g));
-  std::vector<std::size_t> pred(num_vertices(g));
-  default_dijkstra_visitor vis;
+  std::vector<boost::default_color_type> color(boost::num_vertices(g));
+  std::vector<std::size_t> pred(boost::num_vertices(g));
+  boost::default_dijkstra_visitor vis;
   std::less<int> compare;
-  closed_plus<int> combine;
-  auto w_map = get(edge_weight, g);
+  boost::closed_plus<int> combine;
+  auto w_map = boost::get(boost::edge_weight, g);
   dag_shortest_paths(g, s, d_map, w_map, &color[0], &pred[0], 
      vis, compare, combine, (std::numeric_limits<int>::max)(), 0);
 #else
-  dag_shortest_paths(g, s, distance_map(d_map));
+  dag_shortest_paths(g, s, boost::distance_map(d_map));
 #endif
 
-  for(const auto& vertex : make_range_pair(vertices(g)))
+  for(const auto& vertex : make_range_pair(boost::vertices(g)))
     if (d_map[vertex] == (std::numeric_limits<int>::max)())
       std::cout << name[vertex] << ": inifinity\n";
     else
