339a496f10ae819c93089360a660da2c8e49bd6c
Murray Cumming <murrayc@murrayc.com>
2017-05-12 14:25:26 +0200

    examples: miles_span (not built): Remove "using namespace boost".

--- example/miles_span.cpp
+++ example/miles_span.cpp
@@ -37,7 +37,6 @@ struct total_length_visitor : public boost::dijkstra_visitor<> {
 
 int main(int argc, char* argv[])
 {
-  using namespace boost;
   Graph* g;
 
   unsigned long n = 100;
@@ -85,16 +84,16 @@ int main(int argc, char* argv[])
    long sp_length = 0;
 
    // Use the "z" utility field for distance.
-   auto d = get(z_property<long>(), g);
+   auto d = boost::get(z_property<long>(), g);
    // Use the "w" property for parent
-   auto p = get(w_property<Vertex*>(), g);
+   auto p = boost::get(w_property<Vertex*>(), g);
    total_length_visitor<Distance> length_vis(sp_length, d);
 
    prim_minimum_spanning_tree(g, p,
-                              distance_map(get(z_property<long>(), g)).
-                              weight_map(get(edge_length_t(), g)). 
+                              boost::distance_map(boost::get(z_property<long>(), g)).
+                              weight_map(boost::get(edge_length_t(), g)). 
                               // Use the "y" utility field for color
-                              color_map(get(y_property<long>(), g)).
+                              color_map(boost::get(y_property<long>(), g)).
                               visitor(length_vis));
 
    printf("  and its minimum spanning tree has length %ld.\n", sp_length);
