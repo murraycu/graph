eb7860b4cb78cae334cac5f6f51c3d14110ce0fa
Murray Cumming <murrayc@murrayc.com>
2017-05-12 14:18:09 +0200

    examples: kruskal-telephone (not built): Remove "using namespace boost".

--- example/kruskal-telephone.cpp
+++ example/kruskal-telephone.cpp
@@ -15,40 +15,39 @@
 int
 main()
 {
-  using namespace boost;
   GraphvizGraph g_dot;
-  read_graphviz("figs/telephone-network.dot", g_dot);
+  boost::read_graphviz("figs/telephone-network.dot", g_dot);
 
-  using Graph = adjacency_list<vecS, vecS, undirectedS, no_property,
-    property<edge_weight_t, int>>;
-  Graph g(num_vertices(g_dot));
-  auto edge_attr_map = get(edge_attribute, g_dot);
-  for (const auto& edge : make_range_pair(edges(g_dot))) {
+  using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, boost::no_property,
+    boost::property<boost::edge_weight_t, int>>;
+  Graph g(boost::num_vertices(g_dot));
+  auto edge_attr_map = boost::get(edge_attribute, g_dot);
+  for (const auto& edge : make_range_pair(boost::edges(g_dot))) {
     int weight = std::stoi(edge_attr_map[edge]["label"]);
-    property<edge_weight_t, int> edge_property(weight);
-    add_edge(source(edge, g_dot), target(edge, g_dot), edge_property, g);
+    boost::property<boost::edge_weight_t, int> edge_property(weight);
+    boost::add_edge(boost::source(edge, g_dot), boost::target(edge, g_dot), edge_property, g);
   }
 
-  std::vector<graph_traits<Graph>::edge_descriptor> mst;
-  using size_type = std::vector<graph_traits<Graph>::edge_descriptor>::size_type;
+  std::vector<boost::graph_traits<Graph>::edge_descriptor> mst;
+  using size_type = std::vector<boost::graph_traits<Graph>::edge_descriptor>::size_type;
   kruskal_minimum_spanning_tree(g, std::back_inserter(mst));
 
-  auto weight = get(edge_weight, g);
+  auto weight = boost::get(boost::edge_weight, g);
   int total_weight = 0;
   for (const auto& edge : mst)
-    total_weight += get(weight, edge);
+    total_weight += boost::get(weight, edge);
   std::cout << "total weight: " << total_weight << std::endl;
 
-  using Vertex = graph_traits<Graph>::vertex_descriptor;
+  using Vertex = boost::graph_traits<Graph>::vertex_descriptor;
   for (const auto& edge : mst) {
-    auto u = source(edge, g), v = target(edge, g);
+    auto u = boost::source(edge, g), v = boost::target(edge, g);
     edge_attr_map[edge(u, v, g_dot).first]["color"] = "black";
   }
   std::ofstream out("figs/telephone-mst-kruskal.dot");
-  auto graph_edge_attr_map = get_property(g_dot, graph_edge_attribute);
+  auto graph_edge_attr_map = boost::get_priority(g_dot, graph_edge_attribute);
   graph_edge_attr_map["color"] = "gray";
   graph_edge_attr_map["style"] = "bold";
-  write_graphviz(out, g_dot);
+  boost::write_graphviz(out, g_dot);
 
   return EXIT_SUCCESS;
 }
