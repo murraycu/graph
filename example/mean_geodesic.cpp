e735ce5964a9bab5ae8d3de58698d146a5aad9e3
Murray Cumming <murrayc@murrayc.com>
2017-05-12 14:24:54 +0200

    examples: mean_geodesic: Remove "using namespace boost".

--- example/mean_geodesic.cpp
+++ example/mean_geodesic.cpp
@@ -15,8 +15,6 @@
 #include "helper.hpp"
 #include "range_pair.hpp"
 
-using namespace boost;
-
 // The Actor type stores the name of each vertex in the graph.
 struct Actor
 {
@@ -24,27 +22,27 @@ struct Actor
 };
 
 // Declare the graph type and its vertex and edge types.
-using Graph = undirected_graph<Actor>;
-using Vertex = graph_traits<Graph>::vertex_descriptor;
-using Edge = graph_traits<Graph>::edge_descriptor;
+using Graph = boost::undirected_graph<Actor>;
+using Vertex = boost::graph_traits<Graph>::vertex_descriptor;
+using Edge = boost::graph_traits<Graph>::edge_descriptor;
 
 // The name map provides an abstract accessor for the names of
 // each vertex. This is used during graph creation.
-using NameMap = property_map<Graph, std::string Actor::*>::type;
+using NameMap = boost::property_map<Graph, std::string Actor::*>::type;
 
 // Declare a matrix type and its corresponding property map that
 // will contain the distances between each pair of vertices.
-using DistanceProperty = exterior_vertex_property<Graph, int>;
+using DistanceProperty = boost::exterior_vertex_property<Graph, int>;
 using DistanceMatrix = DistanceProperty::matrix_type;
 using DistanceMatrixMap = DistanceProperty::matrix_map_type;
 
 // Declare the weight map so that each edge returns the same value.
-using WeightMap = constant_property_map<Edge, int>;
+using WeightMap = boost::constant_property_map<Edge, int>;
 
 // Declare a container and its corresponding property map that
 // will contain the resulting mean geodesic distances of each
 // vertex in the graph.
-using GeodesicProperty = exterior_vertex_property<Graph, float>;
+using GeodesicProperty = boost::exterior_vertex_property<Graph, float>;
 using GeodesicContainer = GeodesicProperty::container_type;
 using GeodesicMap = GeodesicProperty::map_type;
 
@@ -54,7 +52,7 @@ main(int argc, char *argv[])
     // Create the graph and a property map that provides access
     // to the actor names.
     Graph g;
-    NameMap nm(get(&Actor::name, g));
+    NameMap nm(boost::get(&Actor::name, g));
 
     // Read the graph from standad input.
     read_graph(g, nm, std::cin);
@@ -62,7 +60,7 @@ main(int argc, char *argv[])
     // Compute the distances between all pairs of vertices using
     // the Floyd-Warshall algorithm. Note that the weight map is
     // created so that every edge has a weight of 1.
-    DistanceMatrix distances(num_vertices(g));
+    DistanceMatrix distances(boost::num_vertices(g));
     DistanceMatrixMap dm(distances, g);
     WeightMap wm(1);
     floyd_warshall_all_pairs_shortest_paths(g, dm, weight_map(wm));
@@ -70,15 +68,15 @@ main(int argc, char *argv[])
     // Compute the mean geodesic distances for each vertex in
     // the graph and get the average mean geodesic distace (the
     // so-called small-world distance) as a result.
-    GeodesicContainer geodesics(num_vertices(g));
+    GeodesicContainer geodesics(boost::num_vertices(g));
     GeodesicMap gm(geodesics, g);
     auto sw = all_mean_geodesics(g, dm, gm);
 
     // Print the mean geodesic distance of each vertex and finally,
     // the graph itself.
-    for(const auto& vertex : make_range_pair(vertices(g))) {
+    for(const auto& vertex : make_range_pair(boost::vertices(g))) {
         std::cout << std::setw(12) << std::setiosflags(std::ios::left)
-             << g[vertex].name << get(gm, vertex) << std::endl;
+             << g[vertex].name << boost::get(gm, vertex) << std::endl;
     }
     std::cout << "small world distance: " << sw << std::endl;
 
