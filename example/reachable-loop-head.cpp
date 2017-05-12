012c54cdf9fcca19a40706b7ff1c0942258b70cd
Murray Cumming <murrayc@murrayc.com>
2017-05-12 15:46:53 +0200

    examples: reachable-loop-head (not built): Remove "using namespace boost".

--- example/reachable-loop-head.cpp
+++ example/reachable-loop-head.cpp
@@ -22,22 +22,21 @@ main(int argc, char *argv[])
       << std::endl;
     return -1;
   }
-  using namespace boost;
   GraphvizDigraph g;
-  read_graphviz(argv[1], g);
-  graph_traits<GraphvizDigraph>::vertex_descriptor loop_head = 1;
-  using Color = color_traits<default_color_type>;
+  boost::read_graphviz(argv[1], g);
+  boost::graph_traits<GraphvizDigraph>::vertex_descriptor loop_head = 1;
+  using Color = boost::color_traits<boost::default_color_type>;
 
-  std::vector<default_color_type>
-    reachable_from_head(num_vertices(g), Color::white());
-  default_color_type c;
-  depth_first_visit(g, loop_head, default_dfs_visitor(),
-                    make_iterator_property_map(reachable_from_head.begin(),
-                                               get(vertex_index, g), c));
+  std::vector<boost::default_color_type>
+    reachable_from_head(boost::num_vertices(g), Color::white());
+  boost::default_color_type c;
+  boost::depth_first_visit(g, loop_head, boost::default_dfs_visitor(),
+                    boost::make_iterator_property_map(reachable_from_head.begin(),
+                                               boost::get(boost::vertex_index, g), c));
 
-  auto vattr_map = get(vertex_attribute, g);
+  auto vattr_map = boost::get(vertex_attribute, g);
 
-  for (const auto& vertex : make_range_pair(vertices(g)))
+  for (const auto& vertex : make_range_pair(boost::vertices(g)))
     if (reachable_from_head[vertex] != Color::white()) {
       vattr_map[vertex]["color"] = "gray";
       vattr_map[vertex]["style"] = "filled";
@@ -50,7 +49,7 @@ main(int argc, char *argv[])
             << "size=\"3,3\"\n"
             << "ratio=\"fill\"\n"
             << "shape=\"box\"\n";
-  for (const auto& vertex : make_range_pair(vertices(g))) {
+  for (const auto& vertex : make_range_pair(boost::vertices(g))) {
     loops_out << vertex << "[";
     for (auto ai = vattr_map[vertex].begin();
          ai != vattr_map[vertex].end(); ++ai) {
@@ -60,9 +59,9 @@ main(int argc, char *argv[])
     }
     loops_out<< "]";
   }
-  auto eattr_map = get(edge_attribute, g);
-  for (const auto& edge : make_range_pair(edges(g))) {
-    loops_out << source(edge, g) << " -> " << target(edge, g) << "[";
+  auto eattr_map = boost::get(edge_attribute, g);
+  for (const auto& edge : make_range_pair(boost::edges(g))) {
+    loops_out << boost::source(edge, g) << " -> " << boost::target(edge, g) << "[";
     auto& attr_map = eattr_map[edge];
     for (auto eai = attr_map.begin();
          eai != attr_map.end(); ++eai) {
@@ -74,14 +73,14 @@ main(int argc, char *argv[])
   }
   loops_out << "}\n";
 #else
-  get_property(g, graph_graph_attribute)["size"] = "3,3";
-  get_property(g, graph_graph_attribute)["ratio"] = "fill";
-  get_property(g, graph_vertex_attribute)["shape"] = "box";
+  boost::get_priority(g, graph_graph_attribute)["size"] = "3,3";
+  boost::get_priority(g, graph_graph_attribute)["ratio"] = "fill";
+  boost::get_priority(g, graph_vertex_attribute)["shape"] = "box";
 
-  write_graphviz(loops_out, g,
-                 make_vertex_attributes_writer(g),
-                 make_edge_attributes_writer(g),
-                 make_graph_attributes_writer(g));
+  boost::write_graphviz(loops_out, g,
+                 boost::make_vertex_attributes_writer(g),
+                 boost::make_edge_attributes_writer(g),
+                 boost::make_graph_attributes_writer(g));
 #endif
 
 
