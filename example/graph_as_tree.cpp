765dece12be5279bd403a2d0330df1ca60651d73
Murray Cumming <murrayc@murrayc.com>
2017-05-12 21:21:02 +0200

    examples: graph_as_tree (not built): Remove "using namespace boost".

--- example/graph_as_tree.cpp
+++ example/graph_as_tree.cpp
@@ -21,7 +21,7 @@ public:
   template <typename Node, typename Tree> 
   void inorder(Node n, Tree& t)
   {
-    std::cout << get(boost::vertex_name, t)[n];
+    std::cout << boost::get(boost::vertex_name, t)[n];
   }
   template <typename Node, typename Tree> 
   void postorder(Node, Tree&) {
@@ -32,10 +32,9 @@ public:
 
 int main()
 {
-  using namespace boost;
-  using graph_t = adjacency_list<vecS, vecS, directedS, 
-    property<vertex_name_t, std::string>>;
-  using vertex_t = graph_traits<graph_t>::vertex_descriptor;
+  using graph_t = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, 
+    boost::property<boost::vertex_name_t, std::string>>;
+  using vertex_t = boost::graph_traits<graph_t>::vertex_descriptor;
 
   graph_t g;
 
@@ -43,20 +42,20 @@ int main()
     b = add_vertex(g),
     c = add_vertex(g);
 
-  add_edge(a, b, g);
-  add_edge(a, c, g);
+  boost::add_edge(a, b, g);
+  boost::add_edge(a, c, g);
   
-  auto name = get(vertex_name, g);
+  auto name = boost::get(boost::vertex_name, g);
   name[a] = "A";
   name[b] = "B";
   name[c] = "C";
 
-  using parent_map_t = iterator_property_map<std::vector<vertex_t>::iterator,
-    property_map<graph_t, vertex_index_t>::type>;
-  std::vector<vertex_t> parent(num_vertices(g));
+  using parent_map_t = boost::iterator_property_map<std::vector<vertex_t>::iterator,
+    boost::property_map<graph_t, boost::vertex_index_t>::type>;
+  std::vector<vertex_t> parent(boost::num_vertices(g));
   using tree_t = graph_as_tree<graph_t, parent_map_t>;
-  tree_t t(g, a, make_iterator_property_map(parent.begin(), 
-                                            get(vertex_index, g)));
+  tree_t t(g, a, boost::make_iterator_property_map(parent.begin(), 
+                                            boost::get(boost::vertex_index, g)));
 
   tree_printer vis;
   traverse_tree(a, t, vis);
