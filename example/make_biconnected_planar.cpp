1d59dabe6ad3e8f3f1f32eabf81850d389406884
Murray Cumming <murrayc@murrayc.com>
2017-05-12 14:21:24 +0200

    examples: make_biconnected_planar: Remove "using namespace boost".

--- example/make_biconnected_planar.cpp
+++ example/make_biconnected_planar.cpp
@@ -18,44 +18,41 @@
 #include <boost/graph/boyer_myrvold_planar_test.hpp>
 #include "range_pair.hpp"
 
-
-using namespace boost;
-
 int main(int argc, char** argv)
 {
   
-  using graph = adjacency_list<vecS, vecS,
-    undirectedS,
-    property<vertex_index_t, int>,
-    property<edge_index_t, int>>;
+  using graph = boost::adjacency_list<boost::vecS, boost::vecS,
+    boost::undirectedS,
+    boost::property<boost::vertex_index_t, int>,
+    boost::property<boost::edge_index_t, int>>;
 
   graph g(11);
-  add_edge(0,1,g);
-  add_edge(2,3,g);
-  add_edge(3,0,g);
-  add_edge(3,4,g);
-  add_edge(4,5,g);
-  add_edge(5,3,g);
-  add_edge(5,6,g);
-  add_edge(6,7,g);
-  add_edge(7,8,g);
-  add_edge(8,5,g);
-  add_edge(8,9,g);
-  add_edge(0,10,g);
+  boost::add_edge(0,1,g);
+  boost::add_edge(2,3,g);
+  boost::add_edge(3,0,g);
+  boost::add_edge(3,4,g);
+  boost::add_edge(4,5,g);
+  boost::add_edge(5,3,g);
+  boost::add_edge(5,6,g);
+  boost::add_edge(6,7,g);
+  boost::add_edge(7,8,g);
+  boost::add_edge(8,5,g);
+  boost::add_edge(8,9,g);
+  boost::add_edge(0,10,g);
 
 
   //Initialize the interior edge index
-  auto e_index = get(edge_index, g);
-  graph_traits<graph>::edges_size_type edge_count = 0;
-  for(const auto& edge : make_range_pair(edges(g)))
+  auto e_index = boost::get(boost::edge_index, g);
+  boost::graph_traits<graph>::edges_size_type edge_count = 0;
+  for(const auto& edge : make_range_pair(boost::edges(g)))
     put(e_index, edge, edge_count++);
   
   
   //Test for planarity; compute the planar embedding as a side-effect
-  using vec_t = std::vector<graph_traits<graph>::edge_descriptor>;
-  std::vector<vec_t> embedding(num_vertices(g));
-  if (boyer_myrvold_planarity_test(boyer_myrvold_params::graph = g,
-                                   boyer_myrvold_params::embedding = 
+  using vec_t = std::vector<boost::graph_traits<graph>::edge_descriptor>;
+  std::vector<vec_t> embedding(boost::num_vertices(g));
+  if (boost::boyer_myrvold_planarity_test(boost::boyer_myrvold_params::graph = g,
+                                   boost::boyer_myrvold_params::embedding = 
                                      &embedding[0]
                                    )
       )
@@ -63,14 +60,14 @@ int main(int argc, char** argv)
   else
     std::cout << "Input graph is not planar" << std::endl;
   
-  using component_storage_t = std::vector<graph_traits<graph>::edges_size_type>;
-  using component_map_t = iterator_property_map
+  using component_storage_t = std::vector<boost::graph_traits<graph>::edges_size_type>;
+  using component_map_t = boost::iterator_property_map
     < component_storage_t::iterator, 
-      property_map<graph, edge_index_t>::type
+      boost::property_map<graph, boost::edge_index_t>::type
     >;
   
-  component_storage_t component_storage(num_edges(g));
-  component_map_t component(component_storage.begin(), get(edge_index, g));
+  component_storage_t component_storage(boost::num_edges(g));
+  component_map_t component(component_storage.begin(), boost::get(boost::edge_index, g));
   
   std::cout << "Before calling make_biconnected_planar, the graph has "
             << biconnected_components(g, component)
@@ -80,20 +77,20 @@ int main(int argc, char** argv)
 
   // Re-initialize the edge index, since we just added a few edges
   edge_count = 0;
-  for(const auto& edge : make_range_pair(edges(g)))
+  for(const auto& edge : make_range_pair(boost::edges(g)))
     put(e_index, edge, edge_count++);
 
   // Re-size the storage for the biconnected components, since we
   // just added a few edges
   
-  component_storage.resize(num_edges(g));
-  component = component_map_t(component_storage.begin(), get(edge_index,g));
+  component_storage.resize(boost::num_edges(g));
+  component = component_map_t(component_storage.begin(), boost::get(boost::edge_index,g));
 
   std::cout << "After calling make_biconnected_planar, the graph has "
             << biconnected_components(g, component)
             << " biconnected components" << std::endl;
 
-  if (boyer_myrvold_planarity_test(g))
+  if (boost::boyer_myrvold_planarity_test(g))
     std::cout << "Also, the graph is still planar." << std::endl;
   else
     std::cout << "But the graph is not still planar." << std::endl;
