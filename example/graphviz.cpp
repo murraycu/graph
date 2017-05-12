1bf97e1057b35d02b2886c58640b3ba7f91bce37
Murray Cumming <murrayc@murrayc.com>
2017-05-12 11:42:59 +0200

    examples: graphviz: Remove "using namespace boost".

--- example/graphviz.cpp
+++ example/graphviz.cpp
@@ -12,15 +12,13 @@
 #include <fstream>
 #include <boost/graph/iteration_macros.hpp>
 
-using namespace boost;
+using Digraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                              boost::property<boost::vertex_name_t, std::string>,
+                              boost::property<boost::edge_weight_t, double>>;
 
-using Digraph = boost::adjacency_list<vecS, vecS, directedS,
-                              property<vertex_name_t, std::string>,
-                              property<edge_weight_t, double>>;
-
-using Graph = boost::adjacency_list<vecS, vecS, undirectedS,
-                              property<vertex_name_t, std::string>,
-                              property<edge_weight_t, double>>;
+using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
+                              boost::property<boost::vertex_name_t, std::string>,
+                              boost::property<boost::edge_weight_t, double>>;
 
 void test_graph_read_write(const std::string& filename)
 {
@@ -28,19 +26,19 @@ void test_graph_read_write(const std::string& filename)
   BOOST_REQUIRE(in);
 
   Graph g;
-  dynamic_properties dp;
-  dp.property("id", get(vertex_name, g));
-  dp.property("weight", get(edge_weight, g));
-  BOOST_CHECK(read_graphviz(in, g, dp, "id"));
+  boost::dynamic_properties dp;
+  dp.property("id", boost::get(boost::vertex_name, g));
+  dp.property("weight", boost::get(boost::edge_weight, g));
+  BOOST_CHECK(boost::read_graphviz(in, g, dp, "id"));
 
-  BOOST_CHECK(num_vertices(g) == 4);
-  BOOST_CHECK(num_edges(g) == 4);
+  BOOST_CHECK(boost::num_vertices(g) == 4);
+  BOOST_CHECK(boost::num_edges(g) == 4);
   
-  using Vertex = graph_traits<Graph>::vertex_descriptor;
+  using Vertex = boost::graph_traits<Graph>::vertex_descriptor;
 
   std::map<std::string, Vertex> name_to_vertex;
   BGL_FORALL_VERTICES(v, g, Graph) 
-    name_to_vertex[get(vertex_name, g, v)] = v;
+    name_to_vertex[get(boost::vertex_name, g, v)] = v;
 
   // Check vertices
   BOOST_CHECK(name_to_vertex.find("0") != name_to_vertex.end());
@@ -54,21 +52,21 @@ void test_graph_read_write(const std::string& filename)
   BOOST_CHECK(edge(name_to_vertex["foo"], name_to_vertex["bar"], g).second);
   BOOST_CHECK(edge(name_to_vertex["1"], name_to_vertex["bar"], g).second);
 
-  BOOST_CHECK(get(edge_weight, g, 
+  BOOST_CHECK(boost::get(boost::edge_weight, g, 
                   edge(name_to_vertex["0"], name_to_vertex["1"], g).first)
                 == 3.14159);
-  BOOST_CHECK(get(edge_weight, g, 
+  BOOST_CHECK(boost::get(boost::edge_weight, g, 
                   edge(name_to_vertex["1"], name_to_vertex["foo"], g).first)
                 == 2.71828);
-  BOOST_CHECK(get(edge_weight, g, 
+  BOOST_CHECK(boost::get(boost::edge_weight, g, 
                   edge(name_to_vertex["foo"], name_to_vertex["bar"], g).first)
                 == 10.0);
-  BOOST_CHECK(get(edge_weight, g, 
+  BOOST_CHECK(boost::get(boost::edge_weight, g, 
                   edge(name_to_vertex["1"], name_to_vertex["bar"], g).first)
                 == 10.0);
 
   // Write out the graph
-  write_graphviz_dp(std::cout, g, dp, std::string("id"));
+  boost::write_graphviz_dp(std::cout, g, dp, std::string("id"));
 }
 
 int test_main(int, char*[])
