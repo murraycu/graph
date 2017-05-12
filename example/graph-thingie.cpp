ec0cc218a74b47ae68fbfa902bd15c08c1e32a4f
Murray Cumming <murrayc@murrayc.com>
2017-05-12 11:37:01 +0200

    examples: graph-thingie: Remove "using namespace boost".

--- example/graph-thingie.cpp
+++ example/graph-thingie.cpp
@@ -18,54 +18,52 @@
 #include <sstream>
 #include <cstdlib>
 
-using namespace boost;
-
 
 //
 // Create a custom graph properties
 //  (see the documentation for adjacency list)
-struct graph_identifier_t { using kind = graph_property_tag; };
-struct vertex_label_t { using kind = vertex_property_tag; };
+struct graph_identifier_t { using kind = boost::graph_property_tag; };
+struct vertex_label_t { using kind = boost::vertex_property_tag; };
 
 int main() {
 
   // Vertex properties
-  using vertex_p = property<vertex_name_t, std::string,
-    property< vertex_label_t, std::string,
-    property<vertex_root_t, int>>>;  
+  using vertex_p = boost::property<boost::vertex_name_t, std::string,
+    boost::property< vertex_label_t, std::string,
+    boost::property<boost::vertex_root_t, int>>>;  
   // Edge properties
-  using edge_p = property<edge_name_t, std::string>;
+  using edge_p = boost::property<boost::edge_name_t, std::string>;
   // Graph properties
-  using graph_p = property<graph_name_t, std::string,
-    property<graph_identifier_t, std::string>>;
-  // adjacency_list-based type
-  using graph_t = adjacency_list<vecS, vecS, directedS,
+  using graph_p = boost::property<boost::graph_name_t, std::string,
+    boost::property<graph_identifier_t, std::string>>;
+  // boost::adjacency_list-based type
+  using graph_t = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
     vertex_p, edge_p, graph_p>;
 
   // Construct an empty graph and prepare the dynamic_property_maps.
   graph_t graph(0);
-  dynamic_properties dp;
+  boost::dynamic_properties dp;
 
-  auto vname = get(vertex_name, graph);
+  auto vname = boost::get(boost::vertex_name, graph);
   dp.property("node_id",vname);
 
-  auto vlabel = get(vertex_label_t(), graph);
+  auto vlabel = boost::get(vertex_label_t(), graph);
   dp.property("label",vlabel);
 
-  auto root = get(vertex_root, graph);
+  auto root = boost::get(boost::vertex_root, graph);
   dp.property("root",root);
 
-  auto elabel = get(edge_name, graph);
+  auto elabel = boost::get(boost::edge_name, graph);
   dp.property("label",elabel);
 
   // Use ref_property_map to turn a graph property into a property map
-  ref_property_map<graph_t*, std::string> 
-    gname(get_property(graph,graph_name));
+  boost::ref_property_map<graph_t*, std::string> 
+    gname(boost::get_property(graph, boost::graph_name));
   dp.property("name",gname);
 
   // Use ref_property_map to turn a graph property into a property map
-  ref_property_map<graph_t*, std::string> 
-    gid(get_property(graph,graph_identifier_t()));
+  boost::ref_property_map<graph_t*, std::string> 
+    gid(boost::get_property(graph,graph_identifier_t()));
   dp.property("identifier",gid);
   // Sample graph as an istream;
 
@@ -84,18 +82,18 @@ const char* dot =
 
   std::istringstream gvgraph(dot);
 
-  bool status = read_graphviz(gvgraph,graph,dp,"node_id");
+  bool status = boost::read_graphviz(gvgraph,graph,dp,"node_id");
   if (!status) {
     std::cerr << "read_graphviz() failed." << std::endl;
     return -1;
   }
 
-  std::cout << "graph " << get("name",dp,&graph) <<
-      " (" << get("identifier",dp,&graph) << ")\n\n";
+  std::cout << "graph " << boost::get("name",dp,&graph) <<
+      " (" << boost::get("identifier",dp,&graph) << ")\n\n";
 
   for (auto v : make_range_pair(vertices(graph))) {
-    std::cout << "vertex " << get("node_id",dp,v) <<
-      " (" << get("label",dp,v) << ")\n";
+    std::cout << "vertex " << boost::get("node_id",dp,v) <<
+      " (" << boost::get("label",dp,v) << ")\n";
   }
 
   return status ? EXIT_SUCCESS : EXIT_FAILURE;
