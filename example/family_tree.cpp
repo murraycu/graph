779dce98be451d1b29648cd7c15e5463476afb8e
Murray Cumming <murrayc@murrayc.com>
2017-05-12 11:26:21 +0200

    examples: family_tree: Remove "using namespace boost".

--- example/family_tree.cpp
+++ example/family_tree.cpp
@@ -18,24 +18,23 @@ enum family
 int
 main()
 {
-  using namespace boost;
   const char *name[] = { "Jeanie", "Debbie", "Rick", "John", "Amanda",
     "Margaret", "Benjamin"
   };
 
-  adjacency_list <> g(N);
-  add_edge(Jeanie, Debbie, g);
-  add_edge(Jeanie, Rick, g);
-  add_edge(Jeanie, John, g);
-  add_edge(Debbie, Amanda, g);
-  add_edge(Rick, Margaret, g);
-  add_edge(John, Benjamin, g);
+  boost::adjacency_list <> g(N);
+  boost::add_edge(Jeanie, Debbie, g);
+  boost::add_edge(Jeanie, Rick, g);
+  boost::add_edge(Jeanie, John, g);
+  boost::add_edge(Debbie, Amanda, g);
+  boost::add_edge(Rick, Margaret, g);
+  boost::add_edge(John, Benjamin, g);
 
-  auto index_map = get(vertex_index, g);
+  auto index_map = boost::get(boost::vertex_index, g);
 
-  for (const auto& vertex : make_range_pair(vertices(g))) {
+  for (const auto& vertex : make_range_pair(boost::vertices(g))) {
     std::cout << name[get(index_map, vertex)];
-    auto [ai, a_end] = adjacent_vertices(vertex, g);
+    auto [ai, a_end] = boost::adjacent_vertices(vertex, g);
     if (ai == a_end)
       std::cout << " has no children";
     else
