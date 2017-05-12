978b9fac53bdfe8ed9caa06c9418d79100e4e654
Murray Cumming <murrayc@murrayc.com>
2017-05-12 14:34:02 +0200

    examples: prim-example: Remove "using namespace boost".

--- example/prim-example.cpp
+++ example/prim-example.cpp
@@ -13,9 +13,8 @@
 int
 main()
 {
-  using namespace boost;
-  using Graph = adjacency_list<vecS, vecS, undirectedS,
-    property<vertex_distance_t, int>, property<edge_weight_t, int >>;
+  using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
+    boost::property<boost::vertex_distance_t, int>, boost::property<boost::edge_weight_t, int >>;
   using E = std::pair<int, int>;
   const int num_nodes = 5;
   const auto edges = { E(0, 2), E(1, 3), E(1, 4), E(2, 1), E(2, 3),
@@ -24,23 +23,23 @@ main()
   int weights[] = { 1, 1, 2, 7, 3, 1, 1 };
 #if defined(BOOST_MSVC) && BOOST_MSVC <= 1300
   Graph g(num_nodes);
-  auto weightmap = get(edge_weight, g); 
+  auto weightmap = boost::get(boost::edge_weight, g); 
   for (std::size_t j = 0; j < sizeof(edges) / sizeof(E); ++j) {
-    auto [e, inserted] = add_edge(edges[j].first, edges[j].second, g);
+    auto [e, inserted] = boost::add_edge(edges[j].first, edges[j].second, g);
     weightmap[e] = weights[j];
   }
 #else
   Graph g(std::begin(edges), std::end(edges), weights, num_nodes);
 #endif
-  std::vector < graph_traits < Graph >::vertex_descriptor >
-    p(num_vertices(g));
+  std::vector < boost::graph_traits < Graph >::vertex_descriptor >
+    p(boost::num_vertices(g));
 
 #if defined(BOOST_MSVC) && BOOST_MSVC <= 1300
-  auto distance = get(vertex_distance, g);
-  auto indexmap = get(vertex_index, g);
+  auto distance = boost::get(boost::vertex_distance, g);
+  auto indexmap = boost::get(boost::vertex_index, g);
   prim_minimum_spanning_tree
     (g, *vertices(g).first, &p[0], distance, weightmap, indexmap, 
-     default_dijkstra_visitor());
+     boost::default_dijkstra_visitor());
 #else
   prim_minimum_spanning_tree(g, &p[0]);
 #endif
