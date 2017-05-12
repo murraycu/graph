1d5a26fc80b68a85bb4fd74aa9a16a650e643451
Murray Cumming <murrayc@murrayc.com>
2017-05-12 18:03:09 +0200

    examples: transitive_closure: Remove "using namespace boost".

--- example/transitive_closure.cpp
+++ example/transitive_closure.cpp
@@ -17,34 +17,33 @@
 int
 main(int, char *[])
 {
-  using namespace boost;
-  using Name = property<vertex_name_t, char>;
-  using Index = property<vertex_index_t, std::size_t, Name>;
-  using graph_t = adjacency_list<listS, listS, directedS, Index>;
-  using vertex_t = graph_traits<graph_t>::vertex_descriptor;
+  using Name = boost::property<boost::vertex_name_t, char>;
+  using Index = boost::property<boost::vertex_index_t, std::size_t, Name>;
+  using graph_t = boost::adjacency_list<boost::listS, boost::listS, boost::directedS, Index>;
+  using vertex_t = boost::graph_traits<graph_t>::vertex_descriptor;
   graph_t G;
   std::vector<vertex_t> verts(4);
   for (int i = 0; i < 4; ++i)
     verts[i] = add_vertex(Index(i, Name('a' + i)), G);
-  add_edge(verts[1], verts[2], G);
-  add_edge(verts[1], verts[3], G);
-  add_edge(verts[2], verts[1], G);
-  add_edge(verts[3], verts[2], G);
-  add_edge(verts[3], verts[0], G);
+  boost::add_edge(verts[1], verts[2], G);
+  boost::add_edge(verts[1], verts[3], G);
+  boost::add_edge(verts[2], verts[1], G);
+  boost::add_edge(verts[3], verts[2], G);
+  boost::add_edge(verts[3], verts[0], G);
 
   std::cout << "Graph G:" << std::endl;
-  print_graph(G, get(vertex_name, G));
+  boost::print_graph(G, boost::get(boost::vertex_name, G));
 
-  adjacency_list <> TC;
-  transitive_closure(G, TC);
+  boost::adjacency_list <> TC;
+  boost::transitive_closure(G, TC);
 
   std::cout << std::endl << "Graph G+:" << std::endl;
   char name[] = "abcd";
-  print_graph(TC, name);
+  boost::print_graph(TC, name);
   std::cout << std::endl;
 
   std::ofstream out("tc-out.dot");
-  write_graphviz(out, TC, make_label_writer(name));
+  boost::write_graphviz(out, TC, boost::make_label_writer(name));
 
   return 0;
 }
