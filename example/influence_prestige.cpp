ece4b5c3c1d3f54743cfd640865bde0a750e7eb3
Murray Cumming <murrayc@murrayc.com>
2017-05-12 14:08:05 +0200

    examples: influence_prestige: Remove "using namespace boost".

--- example/influence_prestige.cpp
+++ example/influence_prestige.cpp
@@ -15,8 +15,6 @@
 #include "helper.hpp"
 #include "range_pair.hpp"
 
-using namespace boost;
-
 // The Actor type stores the name of each vertex in the graph.
 struct Actor
 {
@@ -24,18 +22,18 @@ struct Actor
 };
 
 // Declare the graph type and its vertex and edge types.
-using Graph = directed_graph<Actor>;
-using Vertex = graph_traits<Graph>::vertex_descriptor;
-using Edge = graph_traits<Graph>::edge_descriptor;
+using Graph = boost::directed_graph<Actor>;
+using Vertex = boost::graph_traits<Graph>::vertex_descriptor;
+using Edge = boost::graph_traits<Graph>::edge_descriptor;
 
 // The name map provides an abstract accessor for the names of
 // each vertex. This is used during graph creation.
-using NameMap = property_map<Graph, std::string Actor::*>::type;
+using NameMap = boost::property_map<Graph, std::string Actor::*>::type;
 
 // Declare a container type for influence and prestige (both
 // of which are degree centralities) and its corresponding
 // property map.
-using CentralityProperty = exterior_vertex_property<Graph, unsigned>;
+using CentralityProperty = boost::exterior_vertex_property<Graph, unsigned>;
 using CentralityContainer = CentralityProperty::container_type;
 using CentralityMap = CentralityProperty::map_type;
 
@@ -45,23 +43,23 @@ main(int argc, char *argv[])
     // Create the graph and a property map that provides
     // access to the actor names.
     Graph g;
-    NameMap nm(get(&Actor::name, g));
+    NameMap nm(boost::get(&Actor::name, g));
 
     // Read the graph from standard input.
     read_graph(g, nm, std::cin);
 
     // Compute the influence for the graph.
-    CentralityContainer influence(num_vertices(g));
+    CentralityContainer influence(boost::num_vertices(g));
     CentralityMap im(influence, g);
     all_influence_values(g, im);
 
     // Compute the influence for the graph.
-    CentralityContainer prestige(num_vertices(g));
+    CentralityContainer prestige(boost::num_vertices(g));
     CentralityMap pm(prestige, g);
     all_prestige_values(g, pm);
 
     // Print the degree centrality of each vertex
-    for(const auto& v : make_range_pair(vertices(g))) {
+    for(const auto& v : make_range_pair(boost::vertices(g))) {
       std::cout << std::setiosflags(std::ios::left) << std::setw(12)
              << g[v].name << "\t"
              << im[v] << "\t"
