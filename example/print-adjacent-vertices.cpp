596de3df4409ca2e61b4ab407ef456f1bdf3adba
Murray Cumming <murrayc@murrayc.com>
2017-05-12 14:35:07 +0200

    examples: print-adjacent-vertices: Remove "using namespace boost".

--- example/print-adjacent-vertices.cpp
+++ example/print-adjacent-vertices.cpp
@@ -12,15 +12,13 @@
 #include <boost/graph/adjacency_list.hpp>
 #include "range_pair.hpp"
 
-using namespace boost;
-
 template <typename Graph, typename VertexNamePropertyMap> void
 read_graph_file(std::istream & graph_in, std::istream & name_in,
                 Graph & g, VertexNamePropertyMap name_map)
 {
-  using size_type = typename graph_traits<Graph>::vertices_size_type;
+  using size_type = typename boost::graph_traits<Graph>::vertices_size_type;
   size_type n_vertices;
-  typename property_traits<VertexNamePropertyMap>::value_type name;
+  typename boost::property_traits<VertexNamePropertyMap>::value_type name;
 
   graph_in >> n_vertices;       // read in number of vertices
   for (size_type i = 0; i < n_vertices; ++i) {  // Add n vertices to the graph
@@ -31,19 +29,19 @@ read_graph_file(std::istream & graph_in, std::istream & name_in,
   size_type src, targ;
   while (graph_in >> src)       // Read in edges
     if (graph_in >> targ)
-      add_edge(src, targ, g);   // add an edge to the graph
+      boost::add_edge(src, targ, g);   // add an edge to the graph
     else
       break;
 }
 
 template <typename Graph, typename VertexNameMap> void
 output_adjacent_vertices(std::ostream & out,
-                         typename graph_traits<Graph>::vertex_descriptor u,
+                         typename boost::graph_traits<Graph>::vertex_descriptor u,
                          const Graph & g, VertexNameMap name_map)
 {
-  out << get(name_map, u) << " -> { ";
-  for (const auto& vertex : make_range_pair(adjacent_vertices(u, g)))
-    out << get(name_map, vertex) << " ";
+  out << boost::get(name_map, u) << " -> { ";
+  for (const auto& vertex : make_range_pair(boost::adjacent_vertices(u, g)))
+    out << boost::get(name_map, vertex) << " ";
   out << "}" << std::endl;
 }
 
@@ -55,7 +53,7 @@ public:
   }
   template <typename Vertex> bool operator()(Vertex u) const
   {
-    return get(m_name_map, u) == m_name;
+    return boost::get(m_name_map, u) == m_name;
   }
 private:
     std::string m_name;
@@ -74,10 +72,10 @@ name_equals(const std::string & str, NameMap name)
 int
 main()
 {
-  using graph_type = adjacency_list < listS,       // Store out-edges of each vertex in a std::list
-    vecS,                       // Store vertex set in a std::vector
-    directedS,                  // The graph is directed
-    property<vertex_name_t, std::string>     // Add a vertex property
+  using graph_type = boost::adjacency_list < boost::listS,       // Store out-edges of each vertex in a std::list
+    boost::vecS,                       // Store vertex set in a std::vector
+    boost::directedS,                  // The graph is directed
+    boost::property<boost::vertex_name_t, std::string>     // Add a vertex property
    >;
 
   graph_type g;                 // use default constructor to create empty graph
@@ -95,14 +93,14 @@ main()
     return -1;
   }
   // Obtain internal property map from the graph
-  auto name_map = get(vertex_name, g);
+  auto name_map = boost::get(boost::vertex_name, g);
   read_graph_file(file_in, name_in, g, name_map);
 
   auto [i, end] = vertices(g);
-  i = std::find_if(i, end, name_equals("dax.h", get(vertex_name, g)));
-  output_adjacent_vertices(std::cout, *i, g, get(vertex_name, g));
+  i = std::find_if(i, end, name_equals("dax.h", boost::get(boost::vertex_name, g)));
+  output_adjacent_vertices(std::cout, *i, g, boost::get(boost::vertex_name, g));
 
-  assert(num_vertices(g) == 15);
-  assert(num_edges(g) == 19);
+  assert(boost::num_vertices(g) == 15);
+  assert(boost::num_edges(g) == 19);
   return 0;
 }
